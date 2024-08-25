f6245c1e31de92c65000e31f537cd9903cb631b8
wisehackermonkey <oranbusiness@gmail.com>
2024-08-25 16:58:16 -0700

    min

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 # stealmyidea.com Source Code
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.run("0.0.0.0", port=3000, debug=True)
